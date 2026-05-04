c767599eaf5e995f9354769f8daf1e89ef528878
rajieth <rajieth2010@gmail.com>
2026-05-04 20:42:10 +0530

    Add files via upload
    report card code

---
+++ main.py
@@ -0,0 +1,14 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name = 'Alice'
+... print(name, type(name))
+... 
+... is_student = True
+... print(is_student, type(is_student))
+... 
+... age = 20
+... print(age, type(age))
+... 
+... score = 80.5
+... print(isinstance(score, float))
+... print(score, type(score))
